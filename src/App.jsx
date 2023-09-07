81e781e5bcb2474040c4969029a03e8710d6e0ad
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-09-07 14:28:30 +0300

    global styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,75 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
 
-import GlobalStyles from "./styles/GlobalStyles";
-import Dashboard from "./pages/Dashboard";
-import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
-import Account from "./pages/Account";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./ui/AppLayout";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Users />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="account" element={<Account />} />
-          </Route>
+      <>
+        <GlobalStyles />
+        <StyledApp>
+          <H1>The wild oasis</H1>
+          <Button type="button" onClick={() => alert("you are check in")}>
+            Check In
+          </Button>
+          <Button type="button" onClick={() => alert("you are check out")}>
+            Check Out
+          </Button>
+          <Input type="number" placeholder="Number of guests..."/>
+        </StyledApp>
+      </>
 
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
   );
 }
 
 export default App;
+
+// import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+//
+// import GlobalStyles from "./styles/GlobalStyles";
+// import Dashboard from "./pages/Dashboard";
+// import Bookings from "./pages/Bookings";
+// import Cabins from "./pages/Cabins";
+// import Users from "./pages/Users";
+// import Settings from "./pages/Settings";
+// import Account from "./pages/Account";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./ui/AppLayout";
+//
+// function App() {
+//   return (
+//     <>
+//       <GlobalStyles />
+//       <BrowserRouter>
+//         <Routes>
+//           <Route element={<AppLayout />}>
+//             <Route index element={<Navigate replace to="dashboard" />} />
+//             <Route path="dashboard" element={<Dashboard />} />
+//             <Route path="bookings" element={<Bookings />} />
+//             <Route path="cabins" element={<Cabins />} />
+//             <Route path="users" element={<Users />} />
+//             <Route path="settings" element={<Settings />} />
+//             <Route path="account" element={<Account />} />
+//           </Route>
+//
+//           <Route path="login" element={<Login />} />
+//           <Route path="*" element={<PageNotFound />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+//
+// export default App;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -49,17 +49,18 @@ const variations = {
 };
 
 const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
   border: none;
   border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
   box-shadow: var(--shadow-sm);
-
-  ${(props) => sizes[props.size]}
-  ${(props) => variations[props.variation]}
+  color: var(--color-brand-50);
+  cursor: pointer;
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
 `;
-
-Button.defaultProps = {
-  variation: "primary",
-  size: "medium",
-};
 
 export default Button;
